c28138393384f7748ef70d0807487eba01f19ba9
23076-sudo <23076@bayfield-high.school.nz>
2026-07-18 14:21:10 +1200

    adding seerr stats to the python code and the html and also improving the design of the website

--- app.py
+++ app.py
@@ -4,27 +4,46 @@ import json
 from dotenv import load_dotenv 
 load_dotenv() 
 
+# jellyfin api urls/keys
 JF_URL = os.getenv("JF")
 JF_API = os.getenv("JF_KEY")
-headers = {
-    "Authorization": f'MediaBrowser Token="{JF_API}",'
+JF_HEADERS = {
+    "Authorization": f'MediaBrowser Token="{JF_API}"'
 }
-response = requests.get(JF_URL, headers=headers)
 
-
-API_Data = response.json()
-
-for key in API_Data:
-    print(key,":", API_Data[key])
+#seerr api urls/keys
+SEERR_URL = os.getenv("SEERR_URL")
+SEERR_KEY = os.getenv("SEERR_KEY")
+SEERR_HEADERS = {
+    "X-API-Key": SEERR_KEY
+}
 
 def jellyfin():
-    response = requests.get(JF_URL, headers=headers)
-    jf_data = response.json()
-    jf_stats = {
-        "MovieCount": jf_data["MovieCount"],
-        "EpisodeCount": jf_data["EpisodeCount"],
-        "SongCount": jf_data["SongCount"]
+    response = requests.get(JF_URL, headers=JF_HEADERS)
+    data = response.json()
+    return {
+        "MovieCount": data["MovieCount"],
+        "EpisodeCount": data["EpisodeCount"],
+        "SongCount": data["SongCount"]
     }
-    with open("./homepage/stats.json", "w") as jf_stat:
-        json.dump(jf_stats, jf_stat)
+
+def seerr():
+    response = requests.get(f"{SEERR_URL}/api/v1/request/count", headers=SEERR_HEADERS)
+    data = response.json()
+    return {
+        "RequestsTotal": data["total"],
+        "RequestsPending": data["pending"],
+        "RequestsApproved": data["approved"],
+    }
+
+def results():
+    stats = {}
+    stats.update(jellyfin())
+    stats.update(seerr())
+    
+    with open("./homepage/stats.json", "w") as f:
+        json.dump(stats, f)
+        print(stats)
 jellyfin()
+seerr()
+results()
